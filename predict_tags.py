93ee33e04dd0577e759bcf3b75d49cd5e8cd20f9
Lars Kjeldgaard <lars_kjeldgaard@hotmail.com>
2021-03-23 13:53:27 +0100

    go

--- predict_tags.py
+++ predict_tags.py
@@ -1,6 +1,7 @@
 import pandas
 from ebpaperboy.utils import clean_text
 from tabloid import Tabloid
+import json
 
 # instantite tabloid
 model = Tabloid()
@@ -12,44 +13,23 @@ df = pandas.read_csv("~/news_zuckerberg.csv")
 
 tags = []
 
-for i in range(5):
+df = df[:4]
+
+for i in range(len(df)):
     print(i)
     story = df.iloc[i]
+    
     texts = [story['article_title'] + ".",
              story['article_lead'],
              story['article_body']]
 
-    texts = [clean_text(text) for text in texts]
-
     article = " ".join(texts)
 
     tag = model.predict_text(article, threshold = 0.1)
+    tag = {'content_id': int(story['content_id']), 'tag': tag}
 
     tags.append(tag)
 
+with open('test.json', 'w') as fp:
+    json.dump(tags , fp)
     
-# predict from docvec
-docvec = np.random.randn(300) # create a random test docvec
-prediction = model.predict(docvec=docvec)
-
-# predict from text, with text output
-text = 'Folk siger, at Jeppe drikker. Men de siger ikke, hvorfor Jeppe drikker.'
-pred = model.predict_text(text, threshold=0.1)
-
-import numpy as np
-from tabloid import Tabloid
-from 
-
-# instantite tabloid
-model = Tabloid()
-
-# download neural network
-model.download_network()
-
-# predict from docvec
-docvec = np.random.randn(300) # create a random test docvec
-prediction = model.predict(docvec=docvec)
-
-# predict from text, with text output
-text = 'Folk siger, at Jeppe drikker. Men de siger ikke, hvorfor Jeppe drikker.'
-predcition = model.predict(text, threshold=0.1)
