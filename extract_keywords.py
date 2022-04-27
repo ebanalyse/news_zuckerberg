26d547c7d2be23a222114300203e134a2fa80c69
Lars Kjeldgaard <lars_kjeldgaard@hotmail.com>
2022-04-27 11:16:46 +0200

    more polished

--- extract_keywords.py
+++ extract_keywords.py
@@ -9,12 +9,33 @@ import awswrangler as wr
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 from nltk.tokenize import sent_tokenize
 import torch
+import boto3
 
-from ebawsconnect.athena import get_query_results_from_athena
+import keywords
 
-logger = logging.getLogger("Data extraction..")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def lookup_articles(word="Mark Zuckerberg"):
+def lookup_articles(word:str="Mark Zuckerberg",
+                    aws_profile:str="jppol-dfp",
+                    add_keyword:bool=True) -> pd.DataFrame:
+    """Look up articles 
+    
+    Look up articles containing specific keyword.
+    
+    Args:
+        word (str): Keyword to look for.
+        aws_profile(str): Optional. Which aws profile
+            to use to connect to jppol dfp. Profile
+            must be set.
+            Defaults to 'jppol-dfp'.
+        add_keyword(bool): Add keyword to data frame
+            as a column.
+        
+    Returns:
+        pd.DataFrame: pandas Data Frame with resulting
+            articles.
+    """
     
     logger.info(f"Looking up articles containing: {word}")
     
@@ -26,7 +47,7 @@ def lookup_articles(word="Mark Zuckerberg"):
     # form query
     query = f"""
     SELECT *
-    FROM dfp_prod_jppol_dsa_shrd.escenic_article
+    FROM escenic_article
     WHERE article_url NOT LIKE '/incoming%'
     AND cms_publication IN ({' ,'.join(publications)})
     AND LENGTH(article_body) >= 100
@@ -36,35 +57,62 @@ def lookup_articles(word="Mark Zuckerberg"):
     LIMIT 100
     """
     
-    # submit query
-    out = get_query_results_from_athena(query, 
-                                        to_df = True,
-                                        force_query = True, 
-                                        profile_name = "jppol-dfp")
-    
-    # compute year from 'first published'
-    out["year"] = [x[:4] for x in out.first_published.values.tolist()]
-    
-    return out
+     # NOTE: aws profile 'jppol-dfp' must be set
+    session = boto3.Session(profile_name=aws_profile, 
+                            region_name="eu-west-1")
 
-def clean_articles(df):
+    # submit query
+    df = wr.athena.read_sql_query(
+            sql=query,
+            database="dfp_prod_jppol_dsa_shrd", 
+            use_threads=True,
+            boto3_session=session,
+            )
     
-    # drop missings
-    na_cols = ['article_lead', 'article_title', 'article_body']
+    # add keyword if opted for
+    if add_keyword:
+        df["keyword"]=word
+    
+    # compute(/overwrite) year from 'first published'
+    df["year"] = [x[:4] for x in df.first_published.values]
+    
+    return df
+
+def clean_articles(df:pd.DataFrame,
+                   na_cols:list=['article_lead', 'article_title', 'article_body']):
+    """
+    Clean articles
+    
+    Remove irrelevant articles based on relevant criterias.
+    
+    Args:
+        df (pd.DataFrame): data frame with articles.
+        na_cols (list): list with names of columns, for which
+            articles are removed, if they are missing.
+            
+    Returns:
+        pd.DataFrame: articles after cleaning.
+    """
+    
+    # reject missings (not articles)
     df = df.dropna(subset=na_cols)  
     
     # remove duplicates
-    # select last modified entry per content_id
+    # only keep last modified entry per content_id
     df = df.sort_values('last_modified').groupby('content_id').tail(1) 
     
     return df
 
+# HELPER FUNCTION FOR TEXT CLEANING
 def clean_text(text: str) -> str:
     """Clean-up text
+    
     Removes nasty stuff from texts in order to prepare them
     for modelling.
+    
     Args:
         text (str): Text to clean.
+    
     Returns:
         str: cleaned text.
     """
@@ -113,82 +161,88 @@ def format_articles(df):
     
     # subset only relevant columns
     df = df[["content_id",
-           "brand",
-           "article_title",
-           "article_lead",
-           "article_body",
-           "clean_text",
-           "first_published",
-           "article_url"]]
+            "brand",
+            "article_title",
+            "article_lead",
+            "article_body",
+            "clean_text",
+            "first_published",
+            "article_url"]]
     
     return(df)
 
-
-def extract_data(keyword,
-                 to_s3=True,
-                 dir_dest="s3://externalresearch/persons"):
+def extract_data_for_keyword(kw:str,
+                             **kwargs) -> pd.DataFrame:
+    """Extract Data for Keyword
     
-    df = lookup_articles(keyword)
+    Extract, clean and format data for keyword.
+    
+    Args:
+        kw (str): keyword to extract data for.
+        kwargs: all optional arguments for 
+            lookup_articles().
+
+    Returns:
+        pd.DataFrame: resulting "clean" articles.
+    """
+    
+    df = lookup_articles(kw, **kwargs)
     df = clean_articles(df)
     df = format_articles(df)
+
+    return df
+
+def extract_data_for_keywords(keywords:list,
+                              to_s3:bool=True,
+                              s3_path:str="s3://externalresearch/lookupkeywords/persons.csv",
+                              **kwargs):
+    
+    logger.info("Extracting data for keywords...")
+    
+    # extract data for all keywords    
+    dfs = [extract_data_for_keyword(kw, add_keyword=True, **kwargs) for kw in tqdm(keywords)]
+    
+    # row bind all articles into one single data frame
+    dfs = pd.concat(dfs)
+    dfs = dfs.reset_index()
     
     if to_s3:
-        if len(df) > 0:
+        logger.info(f"Writing data to S3: {s3_path}")
+        if len(dfs) > 0:
             wr.s3.to_csv(
-                df,
-                path=f"{dir_dest}/{keyword}.csv",
+                dfs,
+                path=s3_path,
                 index=True
                 )
     
-    return df 
+    return dfs 
 
-keywords = ["Mark Zuckerberg"
-, "Sheryl Sandberg" 
-, "Jeff Bezos" 
-, "Sergei Brin"
-, "Larry Page"
-, "Sundar Pichai" 
-, "Tim Cook" 
-, "Bill Gates" 
-, "Jack Ma" 
-, "Elon Musk" 
-, "Ted Sandros" 
-, "Jack Dorsey"
-, "Margrethe Vestager"
-, "Edward Snowden"
-, "Christopher Wylie"
-, "Peter Thiel" 
-, "Steve Jobs" 
-, "Tim Cook"
-, "Rick Hastings"
-, "Travis Kalanick"]
+if __name__ == "__main__":
+    s3_path = "s3://externalresearch/lookupkeywords/"
+    persons_path = s3_path + "persons.csv"  
+    extract_data_for_keywords(keywords.PERSONS_DEMO, s3_path=persons_path)
 
-[extract_data(kw) for kw in tqdm(keywords)]
-
-
-
-
-def predict_text(pipeline, text):
-    
-    labels = {'positiv': 1,
-              'negativ': -1,
-              'neutral': 0}
-    
-    sents = sent_tokenize(text)
-    preds = pipeline(sents)
-    preds_scores = [labels.get(x.get('label')) for x in preds]
-    score = np.mean(preds_scores)
-    
-    return score
-
-tokenizer = AutoTokenizer.from_pretrained("pin/senda")
-model = AutoModelForSequenceClassification.from_pretrained("pin/senda")
-quantized_model = torch.quantization.quantize_dynamic(
-    model, {torch.nn.Linear}, dtype=torch.qint8
-)
-
-# create 'senda' sentiment analysis pipeline 
-senda_pipeline = pipeline('sentiment-analysis', model=quantized_model, tokenizer=tokenizer)
-
-sents = [predict_text(senda_pipeline, text) for text in tqdm(df.clean_text.values.tolist())]
+# def predict_text(pipeline, text):
+#     
+#     labels = {'positiv': 1,
+#               'negativ': -1,
+#               'neutral': 0}
+#     
+#     sents = sent_tokenize(text)
+#     preds = pipeline(sents)
+#     preds_scores = [labels.get(x.get('label')) for x in preds]
+#     score = np.mean(preds_scores)
+#     
+#     return score
+# 
+# tokenizer = AutoTokenizer.from_pretrained("pin/senda")
+# model = AutoModelForSequenceClassification.from_pretrained("pin/senda")
+# quantized_model = torch.quantization.quantize_dynamic(
+#     model, {torch.nn.Linear}, dtype=torch.qint8
+# )
+# 
+# # create 'senda' sentiment analysis pipeline 
+# senda_pipeline = pipeline('sentiment-analysis', model=quantized_model, tokenizer=tokenizer)
+# 
+# sents = [predict_text(senda_pipeline, text) for text in tqdm(df.clean_text.values.tolist())]
 
---
+++ keywords.py
@@ -0,0 +1,29 @@
+# person keywords
+PERSONS = [
+    "Mark Zuckerberg"
+    , "Sheryl Sandberg" 
+    , "Jeff Bezos" 
+    , "Sergei Brin"
+    , "Larry Page"
+    , "Sundar Pichai" 
+    , "Tim Cook" 
+    , "Bill Gates" 
+    , "Jack Ma" 
+    , "Elon Musk" 
+    , "Ted Sandros" 
+    , "Jack Dorsey"
+    , "Margrethe Vestager"
+    , "Edward Snowden"
+    , "Christopher Wylie"
+    , "Peter Thiel" 
+    , "Steve Jobs" 
+    , "Tim Cook"
+    , "Rick Hastings"
+    , "Travis Kalanick"
+    ]
+
+PERSONS_DEMO = [
+    "Mark Zuckerberg"
+    , "Margrethe Vestager"
+    ]
+
